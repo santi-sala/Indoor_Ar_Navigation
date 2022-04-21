02db9f78fc3f6edaaa4110d7351c5496bd959742
Santiago Sala <pipo0716@gmail.com>
2022-04-21 10:49:56 +0300

    Added some UI for toggling targets and its logic

--- Assets/Scripts/SetNavigationTarget.cs
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -1,17 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class SetNavigationTarget : MonoBehaviour
 {
     [SerializeField]
-    private Camera _topDownCamera;
+    private TMP_Dropdown _navigationTargetDropdown;
     [SerializeField]
-    private GameObject _navTargetObject;
+    private List<Target> _navigationTargetObjects = new List<Target>();
+    
+
+    //private Camera _topDownCamera;
+    //[SerializeField]
+    //private GameObject _navTargetObject;
 
     private NavMeshPath _path;
     private LineRenderer _lineRenderer;
+    private Vector3 _targetPosition = Vector3.zero;
+
 
     private bool _lineToggle = false;
 
@@ -21,22 +28,35 @@ public class SetNavigationTarget : MonoBehaviour
     {
         _path = new NavMeshPath();
         _lineRenderer = transform.GetComponent<LineRenderer>();
+        _lineRenderer.enabled = _lineToggle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began )) 
+        if (_lineToggle && _targetPosition != Vector3.zero) 
         {
-            _lineToggle = !_lineToggle;
-        }
-
-        if (_lineToggle)
-        {
-            NavMesh.CalculatePath(transform.position, _navTargetObject.transform.position, NavMesh.AllAreas, _path);
+            NavMesh.CalculatePath(transform.position, _targetPosition, NavMesh.AllAreas, _path);
             _lineRenderer.positionCount = _path.corners.Length;
             _lineRenderer.SetPositions(_path.corners);
             _lineRenderer.enabled = true;
         }
+    }
+
+    public void SetCurrentNavigationTarget(int selectedValue)
+    {
+        _targetPosition = Vector3.zero;
+        string _selectedText = _navigationTargetDropdown.options[selectedValue].text;
+        Target _currentTarget = _navigationTargetObjects.Find(selectedTarget => selectedTarget.Name.Equals(_selectedText));
+        if (_currentTarget != null)
+        {
+            _targetPosition = _currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        _lineToggle = !_lineToggle;
+        _lineRenderer.enabled = _lineToggle;
     }
 }
